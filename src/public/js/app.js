5c6b3527c90b618a4f268525787efbd915c2e9ce
Gomjong <kjo19990606@gmail.com>
2023-03-26 23:28:57 +0900

    second commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,25 @@
-hello;
+const socket = new WebSocket(`ws://${window.location.host}`); // app.js(프론트엔드)의 socket은 서버와의 연결을 뜻한다
+
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ");
+});
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,7 +7,23 @@ const app = express();
 app.set("view engine", "pug")
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server})
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket); // sockets [] 배열을 만들어서 누가 들어왔는지 알려주게끔 만들기 위해 sockets 만듬 
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from the Browser") );
+    socket.on("message", (message) => {
+        sockets.forEach(aSocket => aSocket.send(message.toString())); // 각 브라우저를 aSocket이라고함
+    });
+});
+
+server.listen(3000, handleListen)
